48cf139159c8e852a2035e35038923df31af971b
Sachin Maurya <kumarsachin67671@gmail.com>
2024-08-03 00:30:37 +0530

    Sets in dart

---
+++ bin/day4/sets.dart
@@ -0,0 +1,17 @@
+class Test {
+  add() {
+    Set items = {
+      1,
+      2,
+      3,
+      4,
+      5,
+    };
+    print(items);
+  }
+}
+
+void main() {
+  Test obj = Test();
+  obj.add();
+}
