e739fa3651688892252572ac0292d14d95d62a80
Sachin Maurya <kumarsachin67671@gmail.com>
2024-08-03 21:14:08 +0530

    map program

---
+++ bin/day5/map.dart
@@ -0,0 +1,11 @@
+class Test {
+  add() {
+    Map<String, String> mapdata = {"name": "Sachin", "SirName": "Dave"};
+    print(mapdata);
+  }
+}
+
+void main() {
+  Test obj = Test();
+  obj.add();
+}
