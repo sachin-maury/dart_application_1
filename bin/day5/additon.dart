1addcecd245c7def090895cba1f6a100750183cf
Sachin Maurya <kumarsachin67671@gmail.com>
2024-08-03 23:14:57 +0530

    practice

---
+++ bin/day5/additon.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+class Addition {
+  static add() {
+    stdout.write("1.Enter your input :");
+    int num1 = int.parse(stdin.readLineSync()!);
+    stdout.write("2.Enter your input :");
+    int num2 = int.parse(stdin.readLineSync()!);
+    int sum = num1 + num2;
+    print(sum);
+  }
+}
---
+++ bin/day5/test.dart
@@ -0,0 +1,12 @@
+import 'dart:js_interop';
+
+class Test {
+  test() {
+    Addition.add();
+  }
+}
+
+void main() {
+  Test obj = Test();
+  obj.test();
+}
