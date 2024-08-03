87e75f831e254c9de00cb09fbaa637e5f37e9a7c
Sachin Maurya <kumarsachin67671@gmail.com>
2024-08-03 22:46:43 +0530

    practice

---
+++ bin/day5/practice2.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+class Addition {
+  sub() {
+    print("sub");
+  }
+
+  mul() {
+    print("mul");
+  }
+
+  Addition() {
+    stdout.write("1.Enter your input :");
+    int num1 = int.parse(stdin.readLineSync()!);
+    stdout.write("2.Enter your input :");
+    int num2 = int.parse(stdin.readLineSync()!);
+    int sum = num1 + num2;
+    print(sum);
+  }
+}
+
+void main() {
+  Addition obj = Addition();
+  obj.sub();
+  obj.mul();
+}
