8f5f1dd460d4264fe47935f34f7f1ccc7de38a46
Sachin Maurya <kumarsachin67671@gmail.com>
2024-08-04 11:02:08 +0530

    Super Constructor

---
+++ bin/day6/superCon.dart
@@ -0,0 +1,14 @@
+class Super{
+  uper(){
+    print("Parent Constructor!");
+  }
+}
+class Child extends Super{
+  child(){
+    print("Child Constructor!");
+  }
+}
+void main(){
+  Child obj = Child();
+  
+}
