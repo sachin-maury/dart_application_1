98256d6d6faa95a848aaa63960dec20a823eb010
Sachin Maurya <kumarsachin67671@gmail.com>
2024-08-04 14:18:50 +0530

    getter setter

---
+++ bin/day6/practiceget_set.dart
@@ -0,0 +1,15 @@
+class A {
+  var name;
+  void set cusset(String name) {
+    this.name = name;
+  }
+
+  String get Cusset {
+    return name;
+  }
+}
+
+void main() {
+  A obj = A();
+  obj.cusset = "Sachin";
+}
