ca50f354470d2a18e2be70c1a83092677e461139
Sachin Maurya <kumarsachin67671@gmail.com>
2024-08-05 11:39:24 +0530

    practice

---
+++ bin/day7/practice.dart
@@ -0,0 +1,14 @@
+class Test {
+  add() async {
+    Future.delayed(Duration(seconds: 2), () => print("\nLine2"));
+  }
+}
+
+void main() {
+  Test obj = Test();
+  print("\nLine1");
+  obj.add();
+  print("\nLine3");
+  
+}
+// USING FOR DELAY PURPOSE
